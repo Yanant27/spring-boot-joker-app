5a8b625e5b8a192e72678a9926b4b49a66a38e73
Yanant27 <htooyanant.hyk@gmail.com>
2021-12-06 00:29:18 +0630

    Change html element, declare private field for ChuckNorrisQuotes.

--- src/main/java/hyk/springframework/springbootjokerapp/service/JokeServiceImpl.java
+++ src/main/java/hyk/springframework/springbootjokerapp/service/JokeServiceImpl.java
@@ -8,6 +8,12 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class JokeServiceImpl implements JokeService {
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
     @Override
     public String getJokes() {
         return new ChuckNorrisQuotes().getRandomQuote();
